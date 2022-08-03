9812da3144b1a09c002d1aea4ec51a74942fb246
Stacy Lofton <lofton713@gmail.com>
2022-08-03 10:59:57 -0500

    list and retrieve working

--- levelup/urls.py
+++ levelup/urls.py
@@ -17,6 +17,14 @@ from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
 from levelupapi.views import register_user, login_user
+from rest_framework import routers
+from levelupapi.views import GameTypeView, GameView, EventView
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'gametypes', GameTypeView, 'gametype')
+router.register(r'games', GameView, 'game')
+router.register(r'events', EventView, 'event')
+
 
 urlpatterns = [
     # Requests to http://localhost:8000/register will be routed to the register_user function
@@ -24,4 +32,5 @@ urlpatterns = [
     # Requests to http://localhost:8000/login will be routed to the login_user function
     path('login', login_user),
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
