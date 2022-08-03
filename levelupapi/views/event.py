2b1b9675aba6c4a6529ef29f92c598ccb5ac7469
Stacy Lofton <lofton713@gmail.com>
2022-08-03 15:49:48 -0500

    Post Event and Game up and running

--- levelupapi/views/event.py
+++ levelupapi/views/event.py
@@ -4,6 +4,8 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
 from levelupapi.models import Event
+from levelupapi.models.game import Game
+from levelupapi.models.gamer import Gamer
 
 
 class EventView(ViewSet):
@@ -37,6 +39,25 @@ class EventView(ViewSet):
             events = events.filter(game_id=game_id)
         
         serializer = EventSerializer(events, many=True)
+        return Response(serializer.data)
+    
+    def create(self, request):
+        """Handle POST operations
+
+        Returns
+            Response -- JSON serialized game instance
+        """
+        gamer = Gamer.objects.get(user=request.auth.user)
+        game = Game.objects.get(pk=request.data["game"])
+
+        event = Event.objects.create(
+            description=request.data["description"],
+            date=request.data["date"],
+            time=request.data["time"],
+            game=game,
+            organizers=gamer
+        )
+        serializer = EventSerializer(event)
         return Response(serializer.data)
     
 class EventSerializer(serializers.ModelSerializer):
--- levelupapi/views/game.py
+++ levelupapi/views/game.py
@@ -4,6 +4,8 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
 from levelupapi.models import Game
+from levelupapi.models.game_type import Game_type
+from levelupapi.models.gamer import Gamer
 
 
 class GameView(ViewSet):
@@ -38,6 +40,26 @@ class GameView(ViewSet):
         serializer = GameSerializer(games, many=True)
         return Response(serializer.data)
     
+    def create(self, request):
+        """Handle POST operations
+
+        Returns
+            Response -- JSON serialized game instance
+        """
+        gamer = Gamer.objects.get(user=request.auth.user)
+        game_type = Game_type.objects.get(pk=request.data["game_type"])
+
+        game = Game.objects.create(
+            title=request.data["title"],
+            maker=request.data["maker"],
+            number_of_players=request.data["number_of_players"],
+            skill_level=request.data["skill_level"],
+            gamer=gamer,
+            game_type=game_type
+        )
+        serializer = GameSerializer(game)
+        return Response(serializer.data)
+        
 class GameSerializer(serializers.ModelSerializer):
     """JSON serializer for games
     """
